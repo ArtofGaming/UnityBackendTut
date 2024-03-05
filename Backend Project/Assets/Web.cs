2333782052b3ae49d2b3f3557795643e05e8d4e6
ArtofGaming <54565543+ArtofGaming@users.noreply.github.com>
2024-03-05 11:49:34 -0700

    init commit

---
+++ Backend Project/Assets/Web.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Web : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetDate());
+        StartCoroutine(GetUsers());
+        StartCoroutine(Login("testuser", "123456"));
+    }
+
+    IEnumerator GetDate()
+    {
+        using(UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackendTutorial/GetDate.php"))
+        {
+            yield return www.SendWebRequest();
+
+            if(www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    IEnumerator GetUsers()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackendTutorial/GetUsers.php"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    }
+
+    IEnumerator Login(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("login", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackendTutorial/Login.php"))
+        {
+            
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}
