c1856d8240215ae1969615950c0bd1c06a564fbf
Campbell Turner <turner4965@gmail.com>
2024-03-26 16:38:15 -0700

    restart

--- Backend Project/Assets/Web.cs
+++ Backend Project/Assets/Web.cs
@@ -8,9 +8,10 @@ public class Web : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(GetDate());
-        StartCoroutine(GetUsers());
+        //StartCoroutine(GetDate());
+        //StartCoroutine(GetUsers());
         StartCoroutine(Login("testuser", "123456"));
+        //StartCoroutine(RegisterUser("sparkle", "bestg1rl"));
     }
 
     IEnumerator GetDate()
@@ -53,11 +54,33 @@ public class Web : MonoBehaviour
     {
         WWWForm form = new WWWForm();
         form.AddField("loginUser", username);
-        form.AddField("login", password);
+        form.AddField("loginPass", password);
 
         using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackendTutorial/Login.php"))
         {
             
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+    IEnumerator RegisterUser(string username, string password)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("loginUser", username);
+        form.AddField("loginPass", password);
+
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/UnityBackendTutorial/RegisterUser.php"))
+        {
+
             yield return www.SendWebRequest();
 
             if (www.result == UnityWebRequest.Result.ConnectionError || www.result == UnityWebRequest.Result.ProtocolError)
